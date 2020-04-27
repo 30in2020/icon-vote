795363e140c65856d79c23673e8b0d172633cf40
JH Kim <couldseeme@icon.foundation>
2020-04-28 03:45:47 +0900

    Updates

---
+++ precache-manifest.84d3d7e9affafed42142e448299ac0fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e56d00f9caca6c404e46d0f0e3e6cb2",
+    "url": "/icon-vote/index.html"
+  },
+  {
+    "revision": "4e7f2a9762d1c5f31b5e",
+    "url": "/icon-vote/static/css/2.0e5962cc.chunk.css"
+  },
+  {
+    "revision": "6732a3241eb8c1d44c68",
+    "url": "/icon-vote/static/css/main.60e8533b.chunk.css"
+  },
+  {
+    "revision": "4e7f2a9762d1c5f31b5e",
+    "url": "/icon-vote/static/js/2.c178264f.chunk.js"
+  },
+  {
+    "revision": "0881a3e940813004fa08b1623b00ecff",
+    "url": "/icon-vote/static/js/2.c178264f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6732a3241eb8c1d44c68",
+    "url": "/icon-vote/static/js/main.4384d404.chunk.js"
+  },
+  {
+    "revision": "1a795cda5344631c673d",
+    "url": "/icon-vote/static/js/runtime-main.e998d948.js"
+  },
+  {
+    "revision": "b75a931178708dc5cf198c51d4c6d34a",
+    "url": "/icon-vote/static/media/logo-all@2x.b75a9311.png"
+  }
+]);
